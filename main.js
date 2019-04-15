8a8e4d9600756ded8d95cdafc8e8a911edfe17d0
jeaks03 <mct0mit23m>
2019-04-15 19:34:50 +0300

    Add dark mode

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-/**
- * What are you looking at? 
- * Of course it's empty!
- */
+toggleNightMode = () => {
+    const body = document.body;
+    const darkModeClass = 'dark-mode'
+    const btn = document.querySelector('.dark-mode-button a');
+
+    if(body.classList.contains(darkModeClass)) {
+        btn.innerHTML ='<i class="far fa-moon"></i>Dark Mode';
+        return body.classList.remove(darkModeClass);
+    }
+
+    btn.innerHTML = '<i class="far fa-sun"></i>Light Mode';
+    body.classList.add(darkModeClass);
+}
